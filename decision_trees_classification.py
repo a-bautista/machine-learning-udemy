fd720feb2d84bbdb9adbf8f409b49a2bd34e034d
alexbr9007 <alex.bautista.ramos.90@gmail.com>
2018-05-13 21:54:16 -0600

    Added the last files of the classification chapter.

---
+++ decision_trees_classification.py
@@ -0,0 +1,97 @@
+# !Python 3.5.2
+# Author: Alejandro Bautista Ramos
+
+# Importing the libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.cross_validation import  train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import confusion_matrix
+from matplotlib.colors import ListedColormap
+from sklearn.tree import DecisionTreeClassifier
+
+
+def main():
+    data_processing()
+
+
+def data_processing():
+
+    # ---------------------------------------- Retrieve the dataset --------------------------------------------- #
+    dataset = pd.read_csv("C:\\Users\\abautista\\Desktop\\Machine_Learning_AZ_Template_Folder\\Part 3 - Classification\\Section 19 - Decision Tree Classification\\Social_Network_Ads.csv")
+
+    # take all the columns except the last one for your matrix of features
+    X = dataset.iloc[:, [2,3]].values
+    y = dataset.iloc[: ,4].values
+
+    # ------------------------ Splitting the dataset into Training set and Test set ------------------------------------ #
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state=0)
+
+    # ------------------------------------------ Feature scaling ------------------------------------------------------- #
+    # feature scaling is not necessary in this algorithm but if you do not scale it then your code my break
+    sc_X = StandardScaler()
+
+    # we scale our training set variables to avoid domination of one big variable against the others and then we apply the changes
+    # with fit_transform
+    X_train = sc_X.fit_transform(X_train)
+
+    # we scale the variables in our test set but we do not fit in our test set because we already fit in our training set
+    X_test = sc_X.transform(X_test)
+
+    # ------------------------------------- Fitting Classifier ---------------------------------------------- #
+
+    classifier = DecisionTreeClassifier(criterion='entropy', random_state=0)
+    classifier.fit(X_train, y_train)
+
+    # ----------------------------------- Predicting the Test Set Results -------------------------------------------- #
+
+    y_pred = classifier.predict(X_test)
+    print("Results test: \n", y_test)
+    print("Results prediction: \n", y_pred)
+
+    # ----------------------------------- Making the confusion matrix ----------------------------------------------- #
+
+    # 7 incorrect predictions
+    cm = confusion_matrix(y_test, y_pred)
+    print("Confusion matrix: \n",cm)
+
+    # ---------------------------------- Visualizing the Training set results ---------------------------------------- #
+
+    X_set, y_set = X_train, y_train
+    X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() -1,  stop = X_set[:, 0].max() + 1, step = 0.01),\
+                         np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+    plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+                 alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+    plt.xlim(X1.min(), X1.max())
+    plt.ylim(X2.min(), X2.max())
+    for i, j in enumerate(np.unique(y_set)):
+        plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                    c = ListedColormap(('red', 'green'))(i), label=j)
+    plt.title('Decision trees (Training Set)')
+    plt.xlabel('Age')
+    plt.ylabel('Estimated Salary')
+    plt.legend()
+    plt.show()
+
+    # ---------------------------------- Visualizing the Test set results ---------------------------------------- #
+
+    X_set, y_set = X_test, y_test
+    X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() -1,  stop = X_set[:, 0].max() + 1, step = 0.01),\
+                         np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step=0.01))
+    plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+                 alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+    plt.xlim(X1.min(), X1.max())
+    plt.ylim(X2.min(), X2.max())
+    for i, j in enumerate(np.unique(y_set)):
+        plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                    c = ListedColormap(('red', 'green'))(i), label=j)
+    plt.title('Decision trees (Test Set)')
+    plt.xlabel('Age')
+    plt.ylabel('Estimated Salary')
+    plt.legend()
+    plt.show()
+
+if __name__ == "__main__":
+    main()
---
+++ random_forest_classification.R
@@ -0,0 +1,93 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: abautista
+# Created on: 5/12/2018
+
+
+
+# ---------------------------------------------- Retrieve the dataset ------------------------------------ #
+
+dataset = read.csv("C:\\Users\\abautista\\Desktop\\Machine_Learning_AZ_Template_Folder\\Part 3 - Classification\\Section 19 - Decision Tree Classification\\Social_Network_Ads.csv")
+
+dataset = dataset[,3:5]#Age, Salary and Purchased columns
+
+
+# --------------------------------------- encoding the target feature ------------------------------------ #
+
+dataset$Purchased = factor(dataset$Purchased, levels = c(0,1))
+
+library(caTools) # necessary library for doing the split
+set.seed(123)
+split = sample.split(dataset$Purchased, SplitRatio = 0.75)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
+
+# ------------------------ Feature scaling ------------------------------------ #
+# feature scaling is not necessary in this algorithm but if you do not scale it then your code my break
+training_set[-3] = scale(training_set[-3])
+test_set[-3] = scale(test_set[-3])
+View(training_set)
+View(test_set)
+
+# ---------------- Fit classifier to the training set ------------------- #
+
+install.packages('randomForest')
+library(randomForest)
+classifier = randomForest(
+             x = training_set[-3],
+             y = training_set$Purchased,
+             ntree = 50 )
+
+# ----------------------- Predicting the Test set results ----------------------- #
+
+y_pred = predict(classifier, newdata = test_set[-3], type='class') #remove the last column which is the dependent variable
+
+# ----------------------- Making the confusion matrix -------------------------- #
+
+cm = table(test_set[, 3], y_pred) # 10 incorrect predictions
+
+# ----------------------- Visualizing the Training set results -------------------- #
+
+install.packages('ElemStatLearn')
+library(ElemStatLearn)
+set = training_set
+X1 = seq(min(set[, 1]) -1, max(set[,1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) -1, max(set[,2]) + 1, by = 0.01)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('Age', 'EstimatedSalary')
+y_grid = predict(classifier, newdata = grid_set, type='class')
+plot(set[, -3],
+     main = 'Random Forest (Training set)',
+     xlab = 'Age', ylab = 'Estimated Salary',
+     xlim =  range(X1), ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
+#This lines must be executed at the very end, so you can see the different points
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
+
+# ----------------------- Visualizing the Test set results -------------------- #
+
+install.packages('ElemStatLearn')
+library(ElemStatLearn)
+set = test_set
+X1 = seq(min(set[, 1]) -1, max(set[,1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) -1, max(set[,2]) + 1, by = 0.01)
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('Age', 'EstimatedSalary')
+y_grid = predict(classifier, newdata = grid_set, type='class')
+plot(set[, -3],
+     main = 'Random Forest (Test set)',
+     xlab = 'Age', ylab = 'Estimated Salary',
+     xlim =  range(X1), ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
+#This lines must be executed at the very end, so you can see the different points
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
+
+
+# ---------------------- Plot the decision tree ------------------------------- #
+
+plot(classifier)
+text(classifier)
+
+
